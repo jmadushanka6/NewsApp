11befd12b530893c335332fa85cfd9c9b9293bd7
jmadushanka6 <janitha.pm6@gmail.com>
2025-06-29 01:10:22 +0200

    Use random Unsplash images for news items

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -14,24 +14,24 @@ export class NewsService {
       title: 'Sunny Weather Expected Tomorrow',
       preview: 'Meteorologists predict sunny skies',
       content: 'Detailed weather forecast goes here...',
-      image: 'https://ichef.bbci.co.uk/images/ic/1024xn/p0l99668.jpg.webp',
-      bigImage: 'https://ichef.bbci.co.uk/images/ic/1024xn/p0l99668.jpg.webp'
+      image: 'https://source.unsplash.com/random/400x300?sig=1',
+      bigImage: 'https://source.unsplash.com/random/800x600?sig=1'
     },
     {
       id: 2,
       title: 'New Electric Car Model Released',
       preview: 'Automaker unveils latest EV',
       content: 'Full article about the new electric car...',
-      image: 'https://ichef.bbci.co.uk/images/ic/1024xn/p0l99668.jpg.webp',
-      bigImage: 'https://ichef.bbci.co.uk/images/ic/1024xn/p0l99668.jpg.webp'
+      image: 'https://source.unsplash.com/random/400x300?sig=2',
+      bigImage: 'https://source.unsplash.com/random/800x600?sig=2'
     },
     {
       id: 3,
       title: 'City Council Approves Park Renovation',
       preview: 'Local park to get major facelift',
       content: 'Details about the renovation project...',
-      image: 'https://ichef.bbci.co.uk/images/ic/1024xn/p0l99668.jpg.webp',
-      bigImage: 'https://ichef.bbci.co.uk/images/ic/1024xn/p0l99668.jpg.webp'
+      image: 'https://source.unsplash.com/random/400x300?sig=3',
+      bigImage: 'https://source.unsplash.com/random/800x600?sig=3'
     }
   ];
 
