98570c8e185a7e45bf44079ca36e0945ea9dd4c0
jmadushanka6 <janitha.pm6@gmail.com>
2025-06-28 21:19:10 +0200

    feat: improve header responsiveness

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent { }
+export class HeaderComponent {
+  isMenuOpen = false;
+  isHidden = false;
+  private lastScrollY = 0;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll(): void {
+    const currentY = window.scrollY;
+    if (currentY > this.lastScrollY && currentY > 50) {
+      this.isHidden = true;
+    } else {
+      this.isHidden = false;
+    }
+    this.lastScrollY = currentY;
+  }
+}
